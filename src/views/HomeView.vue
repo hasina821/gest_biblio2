294d35aa552f9df6174c362d9c31d4893cf39aa8
hasina821 <rmnary.mr@gmail.com>
2022-07-07 15:50:29 +0200

    first-setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-yellow-600 py-4 px-4 w-1/2 rounded-lg">
+    <h1 class="text-white"> Hasina RAMAMY</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
