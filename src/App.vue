a545cd3b07c7a6104dbc232e76e33d72c67cf4a1
hasina821 <rmnary.mr@gmail.com>
2022-07-08 22:36:16 +0200

    second-setup

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <template>
-  <nav>
-    <Navbar/>
-  </nav>
   <router-view/>
 </template>
-<script>
-import Navbar from "./components/Navbar/Navbar.vue"
-export default{
-  name:'App',
-  components:{
-    Navbar
-  }
-}
-</script>
-
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="bg-yellow-600 py-4 px-4 w-1/2 rounded-lg">
-    <h1 class="text-white"> Hasina RAMAMY</h1>
+  <div>
+     <Navbar/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Navbar from "@/components/Navbar/Navbar.vue"
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+     name:'Homepage',
+     components:{
+          Navbar
+     }
+
 }
 </script>
+
+<style>
+
+</style>
